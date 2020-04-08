4a011b0f3d0b06c6a83b794c336ed8a009c28a49
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-08 16:53:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(x,y) {
+  return x + y
+}
+
+add(1,80)
+
+function subtract(x,y) {
+  return x - y
+}
+
+subtract(60,40)
+
+function multiply(x,y) {
+  return x * y
+}
+
+multiply(2,3.4)
+
+function divide() {
+  return x / y
+}
+
+divide(5.0/2.5)
+
+var number=10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+console.log(number)
+
+add5()
+console.log(number)

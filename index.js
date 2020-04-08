0fc974e1a3175a75bef025a718d68ce45c30bb56
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-08 18:13:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function decrement(n) {
 }
 
 function makeInt(n) {
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  return parseFloat('n')
+  return parseFloat(n)
 }

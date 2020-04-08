66407b2945ec4b2962e143424f187eed68765dc6
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-08 17:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,15 +27,3 @@ var number=10
 function add5() {
   number += 5
 }
-
-function divideBy3() {
-  number /= 3
-}
-
-function divideBy3() {
-  console.log(number)
-}
-
-function add5() {
-  console.log(number)
-}
